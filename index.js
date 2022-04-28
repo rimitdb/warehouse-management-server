76508190d9769543bb9c66566cb3be9a155dbf75
Iqbal <iqbal.du.mis@gmail.com>
2022-04-29 00:39:07 +0600

    all package installed

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+const app = express();
+
+//middleware
+app.use(cors());
+app.use(express());
+
+app.get('/', (req, res) => {
+    res.send('Running Project-11- Server')
+});
+
+app.listen(port, () => {
+    console.log("Listening to port", port);
+})
