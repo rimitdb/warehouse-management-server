cc220e13a4e7e32bf6783cb1f61215e48b31f361
Iqbal <iqbal.du.mis@gmail.com>
2022-05-06 04:14:45 +0600

    Post and Delete API created

--- index.js
+++ index.js
@@ -1,16 +1,70 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 const app = express();
 
 //middleware
 app.use(cors());
-app.use(express());
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Running Project-11- Server')
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.obcrm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const productCollection = client.db("InvMgtDb").collection("warehousedata");
+
+        app.get("/product", async (req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        });
+
+        app.get("/product/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const product = await productCollection.findOne(query);
+            res.send(product);
+        });
+
+        //POST Data
+        app.post("/product", async (req, res) => {
+            const newProduct = req.body;
+            const result = await productCollection.insertOne(newProduct);
+            res.send(result);
+        });
+
+        // Delete 
+
+        app.delete("/product/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await productCollection.deleteOne(query);
+            res.send(result);
+        });
+
+    }
+    finally {
+
+    }
+
+}
+
+run().catch(console.dir);
+app.get("/", (req, res) => {
+    res.send("Running From Project-11- Server")
 });
+
+app.get("/user", (req, res) => {
+    res.send("Hello From User dir")
+})
+
 
 app.listen(port, () => {
     console.log("Listening to port", port);
