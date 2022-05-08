2a4af3985e784e5d7586d1e3354a75092e0e42bc
Iqbal <iqbal.du.mis@gmail.com>
2022-05-08 17:57:43 +0600

    console log remove

--- index.js
+++ index.js
@@ -14,8 +14,6 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.obcrm.mongodb.net/InvMgtDb?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-console.log(uri);
-
 async function run() {
     try {
         await client.connect();
